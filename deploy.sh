304c098856ae6054fdae87aa42d1cca6c98cd98d
凉风有信 <3042146237@qq.com>
2022-07-07 11:11:54 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
- git push -f git@github.com:xs-web-lhdd/vitepress-template.git master
+git push -f git@github.com:xs-web-lhdd/vitepress-template.git master:gh-pages
 
 cd -
